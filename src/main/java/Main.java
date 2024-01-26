93e662cb4c53c2a89928d46e8cd3ff2fe5b50d7d
ykvelandia <velandia416@gmail.com>
2024-01-25 21:25:10 -0500

    Implementing a CSV Reader with Java

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            File file = new File("resources/inventory.csv");
+            Scanner fileScanner = new Scanner(file);
+            //Saltar el encabezado del CSV
+            fileScanner.nextLine();
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                String[] productInfo = line.split(",");
+                System.out.println(productInfo[0]);
+                System.out.println(line);
+                //TODO implement the logic to transform this code so it can convert the information in each line into a Product objet
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/model/Product.java
@@ -0,0 +1,73 @@
+package model;
+
+public class Product {
+
+    private String name;
+
+    private String description;
+
+    private String category;
+
+    private String tags;
+
+    private float price;
+    private String imageUrl;
+
+
+    public Product(String name, String description, String category, String tags, float price, String imageUrl) {
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.tags = tags;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
